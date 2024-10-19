70c7294d1b1b574d37cef333667d0b5ea69dea8d
Vishwa Sudaraka <vishwa25flash@gmail.com>
2024-10-19 16:06:43 +0530

    Add files via upload

---
+++ p11.cpp
@@ -0,0 +1,12 @@
+# include <iostream>
+using namespace std;
+int main ()
+{
+    cout << "Enter your age " << endl;
+    int a;
+    cin >> a;
+    cout << "MY Age is " << a;
+    cout << endl;
+    return 0;
+
+}
